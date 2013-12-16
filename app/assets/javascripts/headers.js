56527e613297ffbc2c5d694cd99026e93201ad6e
Spen Taylor <Spen_@hotmail.co.uk>
2013-12-16 17:41:37 -0500

    added JQ to make expandable headers

--- app/assets/javascripts/headers.js
+++ app/assets/javascripts/headers.js
@@ -1,17 +1,37 @@
 $(document).ready(function(){
 
-    $(".header_test").on("click", function(){
+
+		$('.header_icon').on("click", function(){
    
-      var target = $(this).data('expansion');
+      var targetData = $(this).data('expansion');
 
-      if (target = "user") {
-
-      	console.log('true match');
-      	$('.user_admin_header').toggle();
+      if (targetData == "user") {
+      	console.log("user");
+      	var target = $('.user_admin_header');
+      	target.toggle(200);
+      	$('.messages_fix').toggle(200);
+      } else if (targetData == "messages") {
+      	console.log("messages");
+      	var target = $('.messages_header');
+      	target.toggle(200);
+      	$('.user_fix').toggle(200);
 
       };
 
+      var fix_height = $('.main_header').height() + target.height();
+
+
       
+
+
+
+      if ($(this).css('opacity') == '1') {
+      		$(this).css('opacity', 0.6);
+      } else {
+      		$(this).css('opacity', 1);
+      };
+
+      console.log(fix_height);
         return false;
     });  
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,13 @@ module ApplicationHelper
     end
   end
 
+  def current_message_count
+    msg = current_char.received_messages.length
+    br = current_char.received_battle_requests.length
+
+    msg + br
+  end
+
 	def battle_session
     if current_char.battle_session
 		  current_char.battle_session
